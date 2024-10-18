62738f6225b072111d6754ecad5eb6f5eeca7439
JcReyes29 <j.creyesp29@gmail.com>
2024-10-18 02:04:12 -0500

    Movies-page

---
+++ app/movies/page.jsx
@@ -0,0 +1,18 @@
+
+export default function Movies() {
+    return (
+        <>
+            <header className="bg-gray-700 sm:h-14 md:h-14 shadow-xl mx-auto grid justify-center sm:justify-start sm:px-5 lg:px-40 items-center sm:flex p-2">
+                <h1 className="text-white font-medium text-2xl">Popular Movies</h1>
+            </header>
+            <menu className='tall mx-4 md:mx-40 my-8 overflow-y-auto'>
+                <div className='flex flex-wrap justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 2xl:grid-cols-7 gap-3'>
+                    <div className='flex flex-col gap-1'>
+                        <img width={130} className='' src='https://streamcoimg-a.akamaihd.net/000/165/9/1659-PosterArt-b326059d852397768897083483b44324.jpeg' alt='' />
+                        <h2 className='font-bold text-gray-950 text-center'>Program title</h2>
+                    </div>
+                </div>
+            </menu>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -11,15 +11,15 @@ font-medium text-2xl">Popular Titles</h1>
           <menu className="mx-4 grid sm:grid-cols-3 md:mx-40 my-8 tall">
             <div className="flex gap-4 justify-center sm:justify-center md:justify-start">
               <div className="cursor-pointer flex flex-col gap-2">
-                <div className="active:bg-gray-700 bg-[url('/icons/images/placeholder.png')] bg-gray-900 rounded-md bg-cover bg-center h-44 w-28 flex place-content-center items-center">
+                <a href="/series" className="active:bg-gray-700 bg-[url('/icons/images/placeholder.png')] bg-gray-900 rounded-md bg-cover bg-center h-44 w-28 flex place-content-center items-center">
                   <h2 className="text-2xl text-white font-medium">SERIES</h2>
-                </div>
+                </a>
                 <h2 className="text-sm font-medium text-center">Popular Series</h2>
               </div>
               <div className="cursor-pointer flex flex-col gap-2">
-                <div className="active:bg-gray-700 bg-[url('/icons/images/placeholder.png')] bg-gray-900 rounded-md bg-cover bg-center h-44 w-28 flex place-content-center items-center">
+                <a href="/movies" className="active:bg-gray-700 bg-[url('/icons/images/placeholder.png')] bg-gray-900 rounded-md bg-cover bg-center h-44 w-28 flex place-content-center items-center">
                   <h2 className="text-2xl text-white font-medium">MOVIES</h2>
-                </div>
+                </a>
                 <h2 className="text-sm font-medium text-center">Popular Movies</h2>
               </div>
             </div>
