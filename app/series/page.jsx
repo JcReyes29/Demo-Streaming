fbfce3c603ec58695e3c41f8b33e48513c752131
JcReyes29 <j.creyesp29@gmail.com>
2024-10-21 18:02:25 -0500

    problem-api

--- app/series/page.jsx
+++ app/series/page.jsx
@@ -1,8 +1,27 @@
+'use client'
+import axios from "axios";
 
-import Picture from '@/components/Picture';
+export async function CallApi() {
+    // Llamada archivo json
+    const res = await fetch('sample.json');
+    const posts = await res.json();
+
+    return posts
+}
 
 
-export default function Series() {
+const Picture = ({ item }) => (
+
+    <div key={item.title} className='flex flex-col gap-1'>
+        <img width={130} className='cursor-pointer hover:opacity-85 hover:border-2 hover:border-white' src='' alt='' />
+        <h2 className='font-bold text-gray-950 text-center cursor-pointer'>{item.images.PosterArt.url}</h2>
+    </div>
+)
+
+export default function Series({ }) {
+    axios.get('sample.json').then(({ data }) => console.log(data)).catch((error) => console.log(error.message))
+    const posts = []
+
     return (
         <>
             <header className="bg-gray-700 sm:h-14 md:h-14 shadow-xl mx-auto grid justify-center sm:justify-start sm:px-5 lg:px-40 items-center sm:flex p-2">
@@ -10,7 +29,12 @@ export default function Series() {
             </header>
             <menu className='tall mx-4 md:mx-40 my-8 overflow-y-auto'>
                 <div className='flex flex-wrap justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 2xl:grid-cols-7 gap-3'>
-                    <Picture />
+                    {
+                        posts.map(item => (
+                            <Picture key={item.title} item={item} />
+                        ))
+                    }
+
                 </div>
             </menu>
 
--- components/Picture.jsx
+++ components/Picture.jsx
@@ -4,7 +4,7 @@ export default function Picture() {
         <>
             <div className='flex flex-col gap-1'>
                 <img width={130} className='cursor-pointer hover:opacity-85 hover:border-2 hover:border-white' src='' alt='' />
-                <h2 className='font-bold text-gray-950 text-center cursor-pointer'>Title</h2>
+                <h2 className='font-bold text-gray-950 text-center cursor-pointer'>title</h2>
             </div>
         </>
     )
